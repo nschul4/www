46f040fd4ab9deef47bf15d192e0ddf06d51757f
Neal Schultz <neal.schultz@thomsonreuters.com>
2019-06-03 15:16:18 -0700

    project restructure

--- rsync.sh
+++ rsync.sh
@@ -1,2 +1,2 @@
-rsync -a ~/dev/phaser/phaser3-project-template/dist/* ./phaser3-project-template
-rsync -a ~/dev/phaser/phaser3-template/build/* ./phaser3-template
+rsync -a ~/dev/phaser/phaser3-project-template/dist/* ./phaser/phaser3-project-template
+rsync -a ~/dev/phaser/phaser3-template/build/*        ./phaser/phaser3-template
