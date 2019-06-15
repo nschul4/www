4dccd6217b194e8d04decc526dc4a2a7370c9e59
Neal Schultz <neal.schultz@thomsonreuters.com>
2019-06-15 09:17:54 -0700

    mod7

--- rsync.sh
+++ rsync.sh
@@ -1,2 +1,1 @@
-rsync -a ~/dev/phaser/phaser3-project-template/dist/* ./phaser/phaser3-project-template
-rsync -a ~/dev/phaser/phaser3-template/build/*        ./phaser/phaser3-template
+rsync -a ~/dev/phaser/phaser3-demo-spaceship/build/*        ./phaser/phaser3-template
