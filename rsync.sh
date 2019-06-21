076633d1b7dce9de5bdbf46480ba960d56c240fa
Neal Schultz <neal.schultz@thomsonreuters.com>
2019-06-21 11:23:28 -0700

    new project structure

--- rsync.sh
+++ rsync.sh
@@ -1,1 +1,5 @@
-rsync -a ~/dev/phaser/phaser3-demo-spaceship/build/* ./phaser/phaser3-demo-spaceship
+BASE_DIR=~/dev/phaser/my-examples/
+
+rsync -a $BASE_DIR/spaceship_dist/*   ./phaser/spaceship
+rsync -a $BASE_DIR/orbiter_dist/*   ./phaser/orbiter
+# rsync -a $BASE_DIR/pointerevents_dist/*   ./phaser/pointerevents
