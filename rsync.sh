90ddc8c65788fdda27e46ec7eb87d66996884ca3
Neal Schultz <neal.schultz@thomsonreuters.com>
2019-06-23 10:09:57 -0700

    added lander

--- rsync.sh
+++ rsync.sh
@@ -1,5 +1,6 @@
 BASE_DIR=~/dev/phaser/my-examples/
 
-rsync -a $BASE_DIR/spaceship_dist/*   ./phaser/spaceship
-rsync -a $BASE_DIR/orbiter_dist/*   ./phaser/orbiter
-# rsync -a $BASE_DIR/pointerevents_dist/*   ./phaser/pointerevents
+rsync -a $BASE_DIR/lander_dist/*        ./phaser/lander
+rsync -a $BASE_DIR/orbiter_dist/*       ./phaser/orbiter
+rsync -a $BASE_DIR/pointerevents_dist/* ./phaser/pointerevents
+rsync -a $BASE_DIR/spaceship_dist/*     ./phaser/spaceship
