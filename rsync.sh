83f3d294d6fab40032faab6d03aeef9029eb39e9
Neal Schultz <neal.schultz@thomsonreuters.com>
2019-06-26 13:57:26 -0700

    added rocketenginetest

--- rsync.sh
+++ rsync.sh
@@ -1,6 +1,7 @@
 BASE_DIR=~/dev/phaser/my-examples/
 
-rsync -a $BASE_DIR/lander_dist/*        ./phaser/lander
-rsync -a $BASE_DIR/orbiter_dist/*       ./phaser/orbiter
-rsync -a $BASE_DIR/pointerevents_dist/* ./phaser/pointerevents
-rsync -a $BASE_DIR/spaceship_dist/*     ./phaser/spaceship
+rsync -a $BASE_DIR/lander_dist/*           ./phaser/lander
+rsync -a $BASE_DIR/orbiter_dist/*          ./phaser/orbiter
+rsync -a $BASE_DIR/pointerevents_dist/*    ./phaser/pointerevents
+rsync -a $BASE_DIR/rocketenginetest_dist/* ./phaser/rocketenginetest
+rsync -a $BASE_DIR/spaceship_dist/*        ./phaser/spaceship
