4a967e9fce070d8593755d27546e9977f49aebc6
Neal Schultz <neal.schultz@thomsonreuters.com>
2019-07-06 17:52:04 -0700

    soundpad

--- rsync.sh
+++ rsync.sh
@@ -4,4 +4,5 @@ rsync -a $BASE_DIR/lander_dist/*           ./phaser/lander
 rsync -a $BASE_DIR/orbiter_dist/*          ./phaser/orbiter
 rsync -a $BASE_DIR/pointerevents_dist/*    ./phaser/pointerevents
 rsync -a $BASE_DIR/rocketenginetest_dist/* ./phaser/rocketenginetest
+rsync -a $BASE_DIR/soundpad_dist/*         ./phaser/soundpad
 rsync -a $BASE_DIR/spaceship_dist/*        ./phaser/spaceship
