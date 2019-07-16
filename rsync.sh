93179d20df74d5ffce52d4b2537452f2d59b30dd
Neal Schultz <neal.schultz@thomsonreuters.com>
2019-07-16 16:59:44 -0700

    added polygondrop and rocketcar

--- rsync.sh
+++ rsync.sh
@@ -3,6 +3,8 @@ BASE_DIR=~/dev/phaser/my-examples/
 rsync -a $BASE_DIR/lander_dist/*           ./phaser/lander
 rsync -a $BASE_DIR/orbiter_dist/*          ./phaser/orbiter
 rsync -a $BASE_DIR/pointerevents_dist/*    ./phaser/pointerevents
+rsync -a $BASE_DIR/polygondrop_dist/*      ./phaser/polygondrop
+rsync -a $BASE_DIR/rocketcar_dist/*        ./phaser/rocketcar
 rsync -a $BASE_DIR/rocketenginetest_dist/* ./phaser/rocketenginetest
 rsync -a $BASE_DIR/soundpad_dist/*         ./phaser/soundpad
 rsync -a $BASE_DIR/spaceship_dist/*        ./phaser/spaceship
