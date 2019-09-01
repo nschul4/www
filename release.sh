16bc0147d043312823d4da70b2da71e5857d734b
Neal Schultz <neal.schultz@thomsonreuters.com>
2019-09-01 12:36:19 -0700

    release.sh

--- release.sh
+++ release.sh
@@ -9,16 +9,18 @@ trap err_trap ERR
 
 BASE_DIR=~/dev/phaser/my-examples/
 
+echo Popping over to project directory.
+
 pushd .
 cd $BASE_DIR
 npm run build
 
-echo popping back to original dir.
+echo Popping back to release directory.
 popd
 
-echo syncing...
+echo Syncing...
 
-rsync -a $BASE_DIR/gridrunner_dist/*       ./phaser/gridrunner        || true
+rsync -a $BASE_DIR/hauler_dist/*           ./phaser/hauler            || true
 rsync -a $BASE_DIR/lander_dist/*           ./phaser/lander            || true
 rsync -a $BASE_DIR/orbiter_dist/*          ./phaser/orbiter           || true
 rsync -a $BASE_DIR/pointerevents_dist/*    ./phaser/pointerevents     || true
@@ -28,4 +30,7 @@ rsync -a $BASE_DIR/rocketenginetest_dist/* ./phaser/rocketenginetest  || true
 rsync -a $BASE_DIR/soundpad_dist/*         ./phaser/soundpad          || true
 rsync -a $BASE_DIR/spaceship_dist/*        ./phaser/spaceship         || true
 
-echo done.
+echo "Git status..."
+git status
+
+echo Done.
