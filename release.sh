89260d90e670af3a1663b61307eb231814bd63bc
Neal Schultz <neal@collegenet.com>
2019-11-19 08:39:36 -0800

    release.sh

--- release.sh
+++ release.sh
@@ -7,7 +7,7 @@ function err_trap {
 
 trap err_trap ERR
 
-BASE_DIR=~/dev/phaser-examples/
+BASE_DIR=~/dev/phaser-examples
 
 echo Popping over to project directory.
 
@@ -18,20 +18,25 @@ npm run build
 echo Popping back to release directory.
 popd
 
+function rsyncTemplate {
+    local target=$1
+    rsync -a $BASE_DIR/${target}_dist/* ./phaser/${target} || true
+}
+
 echo Syncing...
 
-rsync -a $BASE_DIR/hauler_dist/*           ./phaser/hauler            || true
-rsync -a $BASE_DIR/lander_dist/*           ./phaser/lander            || true
-rsync -a $BASE_DIR/lander-util_dist/*      ./phaser/lander-util       || true
-rsync -a $BASE_DIR/orbiter_dist/*          ./phaser/orbiter           || true
-rsync -a $BASE_DIR/pointerevents_dist/*    ./phaser/pointerevents     || true
-rsync -a $BASE_DIR/polygondrop_dist/*      ./phaser/polygondrop       || true
-rsync -a $BASE_DIR/rocketcar_dist/*        ./phaser/rocketcar         || true
-rsync -a $BASE_DIR/rocketenginetest_dist/* ./phaser/rocketenginetest  || true
-rsync -a $BASE_DIR/soundpad_dist/*         ./phaser/soundpad          || true
-rsync -a $BASE_DIR/spaceship_dist/*        ./phaser/spaceship         || true
-rsync -a $BASE_DIR/vortex_dist/*           ./phaser/vortex            || true
-rsync -a $BASE_DIR/vortex2_dist/*          ./phaser/vortex2           || true
+rsyncTemplate hauler
+rsyncTemplate lander
+rsyncTemplate lander-util
+rsyncTemplate orbiter
+rsyncTemplate pointerevents
+rsyncTemplate polygondrop
+rsyncTemplate rocketcar
+rsyncTemplate rocketenginetest
+rsyncTemplate soundpad
+rsyncTemplate spaceship
+rsyncTemplate vortex
+rsyncTemplate vortex2
 
 echo "Git status..."
 git status
